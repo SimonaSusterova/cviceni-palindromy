a36d5f4e43571b6226f3df372125f0312b6b9317
Simona Šusterová <simsusterova@gmail.com>
2025-05-12 18:53:37 +0200

    hotovo

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ const textPozpatku = (text) => {
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
+
+
+// console.log('Funguju!')
+
+// const textPozpatku = (text) => {
+// 	return text.split('').reverse().join('')
+// }
+
+// textPozpatku('Ahoj světe') // 'etěvs johA'
+// textPozpatku('Kajak') // 'kajaK'
+
+const polozky = document.querySelectorAll('li')
+polozky.forEach((polozka) => {
+	const text = polozka.textContent.toLocaleLowerCase().replaceAll(' ', '')
+	if (text === textPozpatku(text)) {
+		polozka.classList.add('palindrom')
+	}
+})
